1c6d622530cb46fbe8d24b3fd70ae1060cad6688
IP Mag Dev 2 <mag2@inverseparadox.net>
2021-05-22 23:41:28 +0300

    rewrite with knokout.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,95 +4,146 @@ var priority = $("#priority"),
     createGridButton = $("#createGridButton"),
     grid2 = $('.grid-2'),
     formSelect = $('.form-select'),
-    htmlContent = '',
-    blocksArray = [];
+    htmlContent = ''
 
-priority.on( "input", function(e) {
+priority.on( "input", function() {
   if (this.value.length > 2) {
       this.value = this.value.slice(0,2); 
-  }
-  if (this.value < 0) {
+  } else if (this.value < 10) {
+    this.value = this.value.slice(0,1);
+  } else if (this.value < 0) {
     this.value = 0; 
   }
 });
 priority.on( "focus", function(e) {
   this.value = '';
 });
-formSelect.on("hover", function(e) {
-  formSelect.trigger('click'); 
-})
 
-form.on( "submit", function(e) {
-  e.preventDefault();
-  var boxSize = $('.form-select').val();
-  var priority = $('#priority').val();
-  var listGroup = $('.list-group-horizontal');
-  htmlContent += "<li class='list-group-item me-2'>" +boxSize+ "</li>";
-  listGroup.html(htmlContent);
-  var tempObj = {
-    size: boxSize,
-    priority: priority,
-    width: boxSize.charAt(0),
-    height: boxSize.charAt(2)
+
+
+(function (ko) {
+  var BlocksViewModel = function (blocksList) {
+    this.blockList = blockList;
+    this.newBlockSize = ko.observable('');
+    this.newBlockPriority = ko.observable('');
+    this.blocks = ko.observable(false);
+    this.renderedGrid = ko.observable(false);
+    this.isDisabled = ko.observable(false);
+    var self = this;
+
+    this.addBlock = function () {
+      this.blockList.addBlock(this.newBlockSize(), this.newBlockPriority);
+      this.newBlockSize('');
+      this.newBlockPriority('');
+      this.blocks(this.blockList.blocks);
+    }
+    this.createGrid = function () {
+      this.blockList.createGrid();
+      this.renderedGrid (true);
+      this.isDisabled (true);
+    }
+    this.removeBlock = function (blockObject, event) {
+      self.blockList.removeBlock(blockObject.id);
+      self.blocks(self.blockList.blocks)
+    }
+    this.resetGrid = function () {
+      self.blocks(false);
+      this.renderedGrid (false);
+      this.isDisabled (false);
+      grid2.empty();
+      new Muuri('.grid-2').destroy();
+    }
   }
-  blocksArray.push(tempObj);
-  console.log(blocksArray);
-  priority.value = '0';
-});
 
-createGridButton.on("click", function() {
-  var temp = "";
-  blocksArray.sort(function (a, b) { return b.width - a.width })
-  .sort(function (a, b) { return b.priority - a.priority })
-  .forEach(function( elem ) {
-    switch (elem.size) {
-      case "1х1":
-        temp += "<div class='grid-item'><div class='one'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
-        break;
-      case "1х2":
-        temp += "<div class='grid-item'><div class='one-two'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
-        break;
-      case "1х3":
-        temp += "<div class='grid-item'><div class='one-three'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
-        break;
-      case "2х1":
-        temp += "<div class='grid-item'><div class='two-one'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
-        break;
-      case "2х2":
-        temp += "<div class='grid-item'><div class='two'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
-        break;
-      case "2х3":
-        temp += "<div class='grid-item'><div class='two-three'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
-        break;
-      case "3х1":
-        temp += "<div class='grid-item'><div class='three-one'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
-        break;
-      case "3х2":
-        temp += "<div class='grid-item'><div class='three-two'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
-        break;
-      case "3х3":
-        temp += "<div class='grid-item'><div class='three'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
-        break;
-      case "4х1":
-        temp += "<div class='grid-item'><div class='four-one'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
-        break;
-      default:
-      console.log('createGridButton, unknown size of element')
+  var BlockList = function () {
+    this.blocks = [];
+    this.addBlock = function () {
+      var boxSize = formSelect.val();
+      this.blocks.push({
+        id: this.blocks.length,
+        size: boxSize,
+        priority: priority.val(),
+        width: boxSize.charAt(0),
+        height: boxSize.charAt(2)
+      })
+      console.log(this.blocks);
     }
 
-  });
-  grid2.html(temp);
-  createGrid ();
-
-  console.log(blocksArray);
-});
-
-function createGrid () {
-  var grid = new Muuri('.grid-2', {
-    layout: {
-        fillGaps: true,
-        alignBottom: true
+    this.removeBlock = function (id) {
+      var blockIndex = this.getIndexById(id, this.blocks);
+      console.log('blockIndex');
+      console.log(blockIndex);
+      if (typeof blockIndex !== 'undefined') {
+        this.blocks.splice(blockIndex, 1);
       }
-  });
-  grid.refreshItems().layout();
-}
+    }
+
+    this.getIndexById = function (id, blocks) {
+      var index;
+      for (var i=0, max = blocks.length; i < max; i++) {
+        if (blocks[i].id === id) {
+          index = i;
+          break
+        }
+      }
+      return index
+    }
+
+    this.createGrid = function () {
+      var temp = "",
+          grid,
+          blockForGrid = [].concat(this.blocks);
+
+      blockForGrid.sort(function (a, b) { return b.width - a.width })
+          .sort(function (a, b) { return b.priority - a.priority })
+          .forEach(function( elem ) {
+            switch (elem.size) {
+              case "1х1":
+                temp += "<div class='grid-item'><div class='one'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
+                break;
+              case "1х2":
+                temp += "<div class='grid-item'><div class='one-two'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
+                break;
+              case "1х3":
+                temp += "<div class='grid-item'><div class='one-three'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
+                break;
+              case "2х1":
+                temp += "<div class='grid-item'><div class='two-one'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
+                break;
+              case "2х2":
+                temp += "<div class='grid-item'><div class='two'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
+                break;
+              case "2х3":
+                temp += "<div class='grid-item'><div class='two-three'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
+                break;
+              case "3х1":
+                temp += "<div class='grid-item'><div class='three-one'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
+                break;
+              case "3х2":
+                temp += "<div class='grid-item'><div class='three-two'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
+                break;
+              case "3х3":
+                temp += "<div class='grid-item'><div class='three'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
+                break;
+              case "4х1":
+                temp += "<div class='grid-item'><div class='four-one'><div class='grid-item-descr'>size "+elem.size +'<br> pr.'+ elem.priority+ "</div></div></div>";
+                break;
+              default:
+                console.log('createGridButton, unknown size of element')
+            }
+
+          });
+      grid2.html(temp);
+      grid = new Muuri('.grid-2', {
+        layout: {
+          fillGaps: true,
+          alignBottom: true
+        }
+      });
+      grid.refreshItems().layout();
+      temp = "";
+    }
+  }
+  var blockList = new BlockList();
+  ko.applyBindings(new BlocksViewModel(blockList))
+})(ko)
